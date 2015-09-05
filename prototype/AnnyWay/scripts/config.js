a2061da46b2cf16d3377efa9958a3fc8e65f0343
Kirill Shumilov <Kirill.Shumilov@gmail.com>
2015-09-06 00:34:21 +0200

    Added prototype of AnnyWay

---
+++ prototype/AnnyWay/scripts/config.js
@@ -0,0 +1,27 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('app', ['ui.router'])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('app', {
+                    url: '/app',
+                    abstract: true,
+                    templateUrl: 'templates/main-page.html'
+                })
+                .state('app.preview', {
+                    url: '/preview',
+                    views: {
+                        'header': {
+                            templateUrl: 'templates/header.html'
+                        },
+                        'gallery': {
+                            templateUrl: 'templates/image-previews.html',
+                            controller: 'ImagePreviewsController'
+                        }
+                    }
+                });
+
+            $urlRouterProvider.otherwise('/app/preview');
+        });
+}())
